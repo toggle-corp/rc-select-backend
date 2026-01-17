20bddb307e3a75af6b194b9ea58158dcd59662d9
sudip-khanal <sudipkhanal0777@gmail.com>
2026-01-17 20:34:12 +0545

    chore(cors): allow health-check endpoint in CORS URLs

--- main/settings.py
+++ main/settings.py
@@ -366,7 +366,7 @@ CORS_ALLOWED_ORIGINS = TRUSTED_ORIGINS
 CSRF_TRUSTED_ORIGINS = TRUSTED_ORIGINS
 
 CORS_ALLOW_CREDENTIALS = True
-CORS_URLS_REGEX = r"(^/media/.*$)|(^/graphql/$)"
+CORS_URLS_REGEX = r"(^/media/.*$)|(^/graphql/$)|(^/health-check/$)"
 CORS_ALLOW_METHODS = (
     "DELETE",
     "GET",
