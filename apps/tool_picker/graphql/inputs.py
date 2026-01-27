1c8c9ece704b8a4b5332358bf15079d681cac456
sudip-khanal <sudipkhanal0777@gmail.com>
2026-01-27 13:33:56 +0545

    fixup! feat(tool_picker): add mutation for creating UserAnswer

--- apps/tool_picker/graphql/inputs.py
+++ apps/tool_picker/graphql/inputs.py
@@ -1,8 +1,12 @@
 import strawberry
+import strawberry_django
+
+from apps.tool_picker.models import UserAnswer
 
 
-@strawberry.input()
+@strawberry_django.input(UserAnswer)
 class UserAnswerInput:
     catalog: strawberry.ID
     question: strawberry.ID
     ordinal_value: int | None = strawberry.UNSET
+    selected_options: list[strawberry.ID] | None = strawberry.UNSET
--- apps/tool_picker/serializers.py
+++ apps/tool_picker/serializers.py
@@ -1,13 +1,25 @@
 from rest_framework import serializers
 
-from .models import UserAnswer
+from .models import CheckboxOption, UserAnswer
 
 
 class UserAnswerSerializer(serializers.ModelSerializer):
+    selected_options = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=CheckboxOption.objects.all(),
+        required=False,
+        allow_null=True,
+    )
+    ordinal_value = serializers.IntegerField(
+        required=False,
+        allow_null=True,
+    )
+
     class Meta:
         model = UserAnswer
         fields = (
             "catalog",
             "question",
             "ordinal_value",
+            "selected_options",
         )
