2981871d93c00ae184be1cf9f911225e34003abd
sudip-khanal <sudipkhanal0777@gmail.com>
2026-01-27 11:51:28 +0545

    feat(resources): Add tool filter on case study query

--- apps/resources/graphql/filters.py
+++ apps/resources/graphql/filters.py
@@ -2,13 +2,15 @@ import strawberry
 import strawberry_django
 
 from apps.resources.models import CaseStudy, ContactRequest
+from apps.tool_picker.graphql.filters import ToolFilter
 
 
 @strawberry_django.filters.filter(ContactRequest, lookups=True)
 class ContactRequestFilter:
-    id: strawberry.ID | None
+    id: strawberry.ID | None = strawberry.UNSET
 
 
 @strawberry_django.filters.filter(CaseStudy, lookups=True)
 class CaseStudyFilter:
-    id: strawberry.ID | None
+    id: strawberry.ID | None = strawberry.UNSET
+    tool: ToolFilter | None = strawberry.UNSET
